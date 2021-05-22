0eb60bf0f46933a75cca8951554416caec869312
cavalcant3 <jmatheusoc269@gmail.com>
2021-05-22 11:27:21 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import background from './images/background.jpg'
+import Glasscard from './glasscard/glasscard'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"
+    style={{
+       width: '100vw',
+       height: '100vh',
+       backgroundImage: `url(${background})`,
+       backgroundPosition: 'center',
+       backgroundSize: 'cover', 
+       backgroundRepeat: 'no-repeat',
+       display: 'flex',
+       alignItems: 'center',
+       justifyContent: 'center'
+     }}
+    >
+    <Glasscard />
+      
     </div>
   );
 }
